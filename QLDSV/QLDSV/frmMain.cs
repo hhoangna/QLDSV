b5bff50292936508055522cea8be420b935f7043
HHumorous <hhoangna@gmail.com>
2018-08-17 16:57:25 +0700

    frmLogin

---
+++ QLDSV/QLDSV/frmMain.cs
@@ -0,0 +1,234 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QLDSV
+{
+    public partial class frmMain : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private Form CheckExists(Type ftype)
+        {
+            foreach (Form f in this.MdiChildren)
+                if (f.GetType() == ftype)
+                    return f;
+            return null;
+        }
+
+        private void btnLogin_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            Form frm = this.CheckExists(typeof(frmLogin));
+            if (frm != null) frm.Activate();
+            else
+            {
+                frmLogin f = new frmLogin();
+                f.Show();
+            }
+        }
+
+        private void btnExit_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnLogout_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            Program.currentRole = "";
+            Program.currentUserName = "";
+            Program.currentServer = "";
+            Program.currentPass = "";
+            Program.currentID = "";
+
+            btnLogin.Enabled = true;
+            btnLogout.Enabled = false;
+
+            userID.Text = "UerID";
+            userName.Text = "Name";
+            userRole.Text = "Role";
+
+            initRib(true);
+            initRibGroup(false);
+
+            foreach (Form form in this.MdiChildren)
+            {
+                form.Close();
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            btnLogout.Enabled = false;
+            initRibGroup(false);
+        }
+
+        public void initRibGroup(Boolean isEnable)
+        {
+            ribbonTeacherGroup.Enabled = isEnable;
+            ribbonStudentGroup.Enabled = isEnable;
+            ribbonReportGroup.Enabled = isEnable;
+            ribbonManaGroup.Enabled = isEnable;
+        }
+
+        public void initRib(Boolean isVisible)
+        {
+            ribStudent.Visible = isVisible;
+            ribTeacher.Visible = isVisible;
+            ribManage.Visible = isVisible;
+            ribReport.Visible = isVisible;
+        }
+
+        private void btnDep_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            //Form frm = this.CheckExists(typeof(frmDep));
+            //if (frm != null) frm.Activate();
+            //else
+            //{
+            //    frmDep f = new frmDep();
+            //    f.MdiParent = this;
+            //    f.Show();
+            //}
+        }
+
+        private void btnSubject_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            //Form frm = this.CheckExists(typeof(frmSubjects));
+            //if (frm != null) frm.Activate();
+            //else
+            //{
+            //    frmSubjects f = new frmSubjects();
+            //    f.MdiParent = this;
+            //    f.Show();
+            //}
+        }
+
+        private void btnCLass_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            //Form frm = this.CheckExists(typeof(frmClass));
+            //if (frm != null) frm.Activate();
+            //else
+            //{
+            //    frmClass f = new frmClass();
+            //    f.MdiParent = this;
+            //    f.Show();
+            //}
+        }
+
+        private void btnTeacher_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            //Form frm = this.CheckExists(typeof(frmTeacher));
+            //if (frm != null) frm.Activate();
+            //else
+            //{
+            //    frmTeacher f = new frmTeacher();
+            //    f.MdiParent = this;
+            //    f.Show();
+            //}
+        }
+
+        private void btnStudent_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            //Form frm = this.CheckExists(typeof(frmStudent));
+            //if (frm != null) frm.Activate();
+            //else
+            //{
+            //    frmStudent f = new frmStudent();
+            //    f.MdiParent = this;
+            //    f.Show();
+            //}
+        }
+
+        private void btnExam_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            //Form frm = this.CheckExists(typeof(frmQuestion));
+            //if (frm != null) frm.Activate();
+            //else
+            //{
+            //    frmQuestion f = new frmQuestion();
+            //    f.MdiParent = this;
+            //    f.Show();
+            //}
+        }
+
+        private void btnPoint_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            //Form frm = this.CheckExists(typeof(frmTranscript));
+            //if (frm != null) frm.Activate();
+            //else
+            //{
+            //    frmTranscript f = new frmTranscript();
+            //    f.MdiParent = this;
+            //    f.Show();
+            //}
+        }
+
+        private void btnCreate_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            //Form frm = this.CheckExists(typeof(frmSignUp));
+            //if (frm != null) frm.Activate();
+            //else
+            //{
+            //    frmSignUp f = new frmSignUp();
+            //    f.MdiParent = this;
+            //    f.Show();
+            //}
+        }
+
+        private void btnSign_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            //Form frm = this.CheckExists(typeof(frmRegistration));
+            //if (frm != null) frm.Activate();
+            //else
+            //{
+            //    frmRegistration f = new frmRegistration();
+            //    f.MdiParent = this;
+            //    f.Show();
+            //}
+        }
+
+        private void btnReport_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            //Form frm = this.CheckExists(typeof(frmReport));
+            //if (frm != null) frm.Activate();
+            //else
+            //{
+            //    frmReport f = new frmReport();
+            //    f.MdiParent = this;
+            //    f.Show();
+            //}
+        }
+
+        private void btnTest_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            //Form frm = this.CheckExists(typeof(frmListExam));
+            //if (frm != null) frm.Activate();
+            //else
+            //{
+            //    frmListExam f = new frmListExam();
+            //    f.MdiParent = this;
+            //    f.Show();
+            //}
+        }
+
+        private void btnTry_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            //Form frm = this.CheckExists(typeof(frmListExam));
+            //if (frm != null) frm.Activate();
+            //else
+            //{
+            //    frmListExam f = new frmListExam();
+            //    f.MdiParent = this;
+            //    f.Show();
+            //}
+        }
+    }
+}
